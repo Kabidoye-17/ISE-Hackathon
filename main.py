ab1af4d44ec89e60054b2b0fbeb06c02157c1034
Elle0-0 <andreamichellevaz@gmail.com>
2024-02-17 13:34:33 +0000

    daft locations

--- main.py
+++ main.py
@@ -3,37 +3,35 @@ from bs4 import BeautifulSoup
 
 def scrape_real_estate_listings(url):
     # Send a GET request to the URL
-    response = requests.get(url)
+    #response = requests.get(url)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+    response = requests.get(url, headers=headers)
+
+   
 
     # Check if request was successful
     if response.status_code == 200:
-        # Parse the HTML content
+
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        # Find all the listings on the page
-        listings = soup.find_all('div', class_='listing')
+        listings = soup.find_all('div', class_='Cardstyled__TitleBlockWrapper-nngi4q-5 bsQod')
 
-        # Iterate through each listing and extract relevant information
         for listing in listings:
-            # Extract property title
-            title = listing.find('h2', class_='title').text.strip()
-
+        
             # Extract property price
-            price = listing.find('span', class_='price').text.strip()
+            price = listing.find('h3', class_='TitleBlock__StyledCustomHeading-sc-1avkvav-5 blbeVq').text.strip()
 
             # Extract property location
-            location = listing.find('p', class_='location').text.strip()
+            location = listing.find('h2', class_='TitleBlock__Address-sc-1avkvav-8 dzihyY').text.strip()
 
-            # Print the extracted information
-            print(f"Title: {title}")
+            #print(f"Title: {title}")
             print(f"Price: {price}")
             print(f"Location: {location}")
             print("--------------------------")
     else:
         print("Failed to retrieve webpage.")
-
 # URL of the real estate listings page
-url = 'https://www.zillow.com/homes/'
+url = 'https://www.daft.ie/property-for-rent/limerick/houses'
 
 # Call the function to scrape real estate listings
 scrape_real_estate_listings(url)
